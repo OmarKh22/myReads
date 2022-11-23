1d9b0b4fe0a501a0083774dbbe64273c83f52477
omarKh22 <ok597451@gmail.com>
2022-11-23 14:11:49 +0200

    show all authers

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -8,6 +8,10 @@ const Book = ({id,img,title,authors,setcount}) => {
             setcount((prev)=>prev+1)
             // console.log('update',data)
             //window.location.reload()
+            
+                    // if (val = "currentlyReading") {
+                    //   return val = "currentlyReading"
+                    // }
         }catch(err){
             console.log(err)
         } 
@@ -21,16 +25,17 @@ const Book = ({id,img,title,authors,setcount}) => {
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${img})` }}></div>
                             <div className="book-shelf-changer">
-                              <select defaultValue={'move'} onClick={(e)=>handelSelect(id,e.target.value)}>
+                              <select  onClick={(e)=>handelSelect(id,e.target.value)}>
                                 <option value="move"  disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
                                 <option value="read">Read</option>
+                                <option value="none">None</option>
                               </select>
                             </div>
                           </div>
                           <div className="book-title">{title}</div>
-                          <div className="book-authors">{authors[0]}</div>
+                          <div className="book-authors">{authors}</div>
                         </div>
                       </li>
   )
